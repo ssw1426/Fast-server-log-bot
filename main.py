0baaa189d34b9861d9dd59e3fe53c4f4aed0b930
ssw1426 <80308398+ssw1426@users.noreply.github.com>
2021-03-10 11:19:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 
 client = discord.Client()
+token = "ODE4NjQ0OTcyMTEwMDg2MTY0.YEbEiw.-Jh_BGdqL5ddim6q4LSMqIThxpU"
 
 @client.event
 async def on_ready():
@@ -24,4 +25,4 @@ async def on_message(message):
         embed.add_field(name="서버 온 되면 들어와주세요", value="저희 Next 서버는 패치 및 버그에 최선를 다합니다", inline=False)
         await message.channel.send(embed=embed)
 
-client.run("ODE4NjQ0OTcyMTEwMDg2MTY0.YEbEiw.-Jh_BGdqL5ddim6q4LSMqIThxpU")
+client.run(token)
