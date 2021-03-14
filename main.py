73d60cdd58ede3dc023a3220ea38a3a9aa7edcef
ssw1426 <80308398+ssw1426@users.noreply.github.com>
2021-03-15 02:02:31 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ intents.members = True
 client = discord.Client(intents=intents)
 
 @client.event
+async def on_ready():
+    game = discord.Game('서버 입퇴장 기록')
+    await client.change_presence(status=discord.Status.online, activity=game)
+    print("봇이 온라인으로 변경되었습니다")
+
+@client.event
 async def on_member_join(member):
     embed = discord.Embed(title="[서버 입장 로그]", description=f"{member.mention} 님께서 입장하셨습니다", color=0x62c1cc)
     embed.add_field(name="[ 뉴비 인증 안내 ]", value="?????", inline=False)
