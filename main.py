d7b67c2492bd0082c7ac45bf80d5499b57b91fe7
ssw1426 <80308398+ssw1426@users.noreply.github.com>
2021-03-15 01:03:11 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,12 +7,6 @@ intents.members = True
 client = discord.Client(intents=intents)
 
 @client.event
-async def on_ready():
-    game = discord.Game('입퇴장 로그 확인')
-    await client.change_presence(status=discord.Status.online, activity=game)
-    print("봇이 온라인으로 변경되었습니다")
-
-@client.event
 async def on_member_join(member):
     await client.get_channel(820657549018660864).send(f"{member.mention} 입장함")
 
