e7922b3dfc9d62dca34df5832a2065908f75f30e
ssw1426 <80308398+ssw1426@users.noreply.github.com>
2021-03-15 01:45:52 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ client = discord.Client(intents=intents)
 
 @client.event
 async def on_member_join(member):
-    await client.get_channel(820657549018660864).send("어서오세요!")
     embed = discord.Embed(title="[서버 입장 로그]", description=f"{member.mention} 님께서 입장하셨습니다", color=0x62c1cc)
     embed.add_field(name="[ 뉴비 인증 안내 ]", value="?????", inline=False)
     await client.get_channel(820657549018660864).send(embed=embed)
