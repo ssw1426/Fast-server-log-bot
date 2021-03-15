a3f96c521e779552bc3d5daba8ea4400a51653bb
ssw1426 <80308398+ssw1426@users.noreply.github.com>
2021-03-15 20:38:33 +0900

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ async def on_ready():
 @client.event
 async def on_member_join(member):
     embed = discord.Embed(title="[서버 입장 로그]", description=f"{member.mention} 님께서 입장하셨습니다", color=0x62c1cc)
-    embed.add_field(name="[ 뉴비 인증 안내 ]", value="?????", inline=False)
     embed.set_footer(text=f"{member}")
     await client.get_channel(820657549018660864).send(embed=embed)
 
@@ -25,5 +24,5 @@ async def on_member_remove(member):
     embed.set_footer(text=f"{member}")
     await client.get_channel(820657549018660864).send(embed=embed)
 
-access_token = ["BOT_TOKEN"]
+access_token = os.environ["BOT_TOKEN"]
 client.run(access_token)
